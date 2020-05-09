63c64d15d80deae5290877efe8c5c742602106f1
unknown <wangrong19810806@gmail.com>
2020-05-08 23:00:54 -0400

    add BinaryTree

--- Day07-#993.py
+++ Day07-#993.py
@@ -22,3 +22,16 @@ The number of nodes in the tree will be between 2 and 100.
 Each node has a unique integer value from 1 to 100.
 
 """
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+
+        return
