0ae432f09ef1c9a5398955c5730e76486018ce70
AuroraRW <wangrong19810806@gmail.com>
2020-06-14 19:58:59 -0400

    update

--- Problem771.cs
+++ Problem771.cs
@@ -23,5 +23,17 @@ namespace Algorithm_Leetcode_CSharp
 {
     class Problem771
     {
+        public int NumJewelsInStones(string J, string S)
+        {
+            int count = 0;
+            for(int i=0; i<S.Length; i++)
+            {
+                if(J.Contains(S[i])){
+                    count++;
+                }
+            }
+            return count;
+
+        }
     }
 }
